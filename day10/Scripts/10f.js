edb7dbc5cabd32c65701580d4bc1f72ac304cb00
arunkrishnan29 <arunkrishnan2918@gmail.com>
2026-05-31 23:45:24 +0530

    objects,DOM,(Html,css,js)ref

---
+++ day10/Scripts/10f.js
@@ -0,0 +1,13 @@
+          
+          
+          
+          
+          
+          
+          function check(btn, className){
+                if(btn.classList.contains(className)){
+                    btn.classList.remove(className);
+                } else {
+                    btn.classList.add(className);
+                }
+            }
